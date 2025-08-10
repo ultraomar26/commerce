9f0811f9821005cdc0888ab3cb327cafd4356f5c
ultraomar26 <ultra.our@gmail.com>
2025-08-10 14:12:46 +0300

    feat(templates): add empty template files for auctions feature
    Add initial empty template files for bid, listing, and watchlist functionality to establish the template structure

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create", views.create, name="create"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("closed", views.closed, name="closed"),
+    path("bid", views.bid, name="bid"),
 ]
